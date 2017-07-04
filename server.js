92d4bf011ebb31a4e67ad74e87588a9889597e93
surwase <pratap7684@gmail.com>
2017-07-04 16:58:19 +0530

    setup start heruko

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 var app = express();
+const port  =process.env.PORT || 3000;
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine',  'hbs');
 
@@ -45,6 +46,6 @@ app.get('/bad', (req, res) => {
     errorMessgae: 'Unable to handle request'
   });
 });
-app.listen(3000 , () => {
-  console.log('Server is strated on 3000');
+app.listen(port, () => {
+  console.log(`Server is strated on ${port}`);
 } );
