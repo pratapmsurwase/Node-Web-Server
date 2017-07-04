dbb8bdca7f4d49d870ec91abf73e59a6e21d617f
surwase <pratap7684@gmail.com>
2017-07-04 14:05:13 +0530

    new commit of web server

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine',  'hbs');
+
+ app.use((req, res, next)  => {
+   var now = new Date().toString();
+   var log = `${now}: ${req.method}  ${req.url}` ;
+   console.log(log);
+   fs.appendFile('server.log',  log + '\n' , (error)=>{
+     if (error){
+     cosnole.log('Unable to fetch data');
+   }
+   });
+   next();
+ });
+  //  app.use((req, res, next) => {
+  //    res.render('maintenance.hbs');
+  //  });
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',  (req, res) => {
+res.render('home.hbs', {
+  pageTitle: 'Home Page of Home',
+    welcome: 'WelCome to my site'
+  });
+   });
+app.get('/about',  (req, res)  => {
+  res.render('about.hbs', {
+  pageTitle: 'Home Page of About',
+    welcome: 'WelCome to my site About'
+  });
+  });
+
+   hbs.registerHelper('getCurrentYear',  () => {
+      return new Date().getFullYear();
+   });
+   hbs.registerHelper('screamit', ( ) => {
+     return text.toUpperCase();
+   });
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessgae: 'Unable to handle request'
+  });
+});
+app.listen(3000 , () => {
+  console.log('Server is strated on 3000');
+} );
